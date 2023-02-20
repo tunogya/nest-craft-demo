86a484e751a70bc2ccf6aa58c16ccc1e71f65265
tunogya <tunogya@qq.com>
2023-02-20 18:52:27 +0800

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,135 @@
-import {Button, Heading, HStack, IconButton, Stack, Text} from "@chakra-ui/react";
-import {useState} from "react";
+import {
+  Accordion,
+  AccordionButton, AccordionIcon,
+  AccordionItem, AccordionPanel,
+  Button,
+  Heading,
+  HStack, Popover, PopoverBody, PopoverContent, PopoverHeader, PopoverTrigger,
+  Stack,
+  Text
+} from "@chakra-ui/react";
+import {useRef, useState} from "react";
 import Editor from "@monaco-editor/react";
-import {SmallAddIcon} from "@chakra-ui/icons";
 
 const Home = () => {
   const [text, setText] = useState("");
+  const monacoRef = useRef(null);
+
+  const Operator = [
+    {
+      name: '基础运算符 Basic Operator',
+      children: [
+        {
+          name: '+',
+          value: '+',
+          explain: '+',
+        },
+        {
+          name: '-',
+          value: '-',
+          explain: '-',
+        },
+        {
+          name: '*',
+          value: '*',
+          explain: '*',
+        },
+        {
+          name: '/',
+          value: '/',
+          explain: '/',
+        },
+        {
+          name: '**',
+          value: '**',
+          explain: '**',
+        }
+      ]
+    }, {
+      name: '内置函数 Built-in Function',
+      children: [
+        {
+          name: 'bn()',
+          value: 'bn()',
+          explain: 'bn()',
+        },
+        {
+          name: 'ts()',
+          value: 'ts()',
+          explain: 'ts()',
+        },
+        {
+          name: 'op(int pairIndex)',
+          value: 'op(int pairIndex)',
+          explain: 'op(int pairIndex)',
+        },
+        {
+          name: 'oav(int pairIndex, int count)',
+          value: 'oav(int pairIndex, int count)',
+          explain: 'oav(int pairIndex, int count)',
+        },
+        {
+          name: 'ln(int v)',
+          value: 'ln(int v)',
+          explain: 'ln(int v)',
+        },
+        {
+          name: 'exp(int v)',
+          value: 'exp(int v)',
+          explain: 'exp(int v)',
+        },
+        {
+          name: 'flo(int v)',
+          value: 'flo(int v)',
+          explain: 'flo(int v)',
+        },
+        {
+          name: 'cel(int v)',
+          value: 'cel(int v)',
+          explain: 'cel(int v)',
+        },
+        {
+          name: 'log(int a, int b)',
+          value: 'log(int a, int b)',
+          explain: 'log(int a, int b)',
+        },
+        {
+          name: 'pow(int a, int b)',
+          value: 'pow(int a, int b)',
+          explain: 'pow(int a, int b)',
+        },
+      ]
+    }, {
+      name: '鞅函数 Martingale Function',
+      children: [
+        {
+          name: 'm1(int pairIndex)',
+          value: 'm1(int pairIndex)',
+          explain: '线性函数, m1 = a * x + c\n其中，pairIndex表示目标代币编号(0:ETH, 1:NEST, 2:BTC)',
+        },
+        {
+          name: 'm2(int pairIndex)',
+          value: 'm2(int pairIndex)',
+          explain: '平方函数，m2 = a * x ** 2 + c\n其中，pairIndex表示目标代币编号(0:ETH, 1:NEST, 2:BTC)',
+        },
+        {
+          name: 'm3(int pairIndex)',
+          value: 'm3(int pairIndex)',
+          explain: '倒数函数，m3 = a / x + c\n其中，pairIndex表示目标代币编号(0:ETH, 1:NEST, 2:BTC)',
+        },
+        {
+          name: 'm4(int pairIndex)',
+          value: 'm4(int pairIndex)',
+          explain: '开方函数，m4 = a * x ** 0.5 + c\n其中，pairIndex表示目标代币编号(0:ETH, 1:NEST, 2:BTC)',
+        },
+        {
+          name: 'm5(int pairIndex)',
+          value: 'm5(int pairIndex)',
+          explain: '对数函数，m5 = a * ln(x) + c\n其中，pairIndex表示目标代币编号(0:ETH, 1:NEST, 2:BTC)',
+        }
+      ],
+    }
+  ]
 
   return (
     <Stack p={5} h={'100vh'}>
@@ -13,47 +138,52 @@ const Home = () => {
       </Stack>
       <Heading fontSize={'md'}>0xUSER</Heading>
       <HStack h={'full'} spacing={5}>
-        <Stack minW={'400px'} bg={'gray.100'} h={'full'} p={4} spacing={4}>
-          <Stack>
-            <Text fontSize={'sm'} fontWeight={'semibold'}>基础运算符</Text>
-            <Text fontSize={'sm'}>+</Text>
-            <Text fontSize={'sm'}>-</Text>
-            <Text fontSize={'sm'}>*</Text>
-            <Text fontSize={'sm'}>/</Text>
-            <Text fontSize={'sm'}>**</Text>
-          </Stack>
-
-          <Stack>
-            <Text fontSize={'sm'} fontWeight={'semibold'}>内置函数</Text>
-            <Text fontSize={'sm'}>bn()</Text>
-            <Text fontSize={'sm'}>ts()</Text>
-            <Text fontSize={'sm'}>op(int pairIndex)</Text>
-            <Text fontSize={'sm'}>oav(int pairIndex, int count)</Text>
-            <Text fontSize={'sm'}>ln(int v)</Text>
-            <Text fontSize={'sm'}>exp(int v)</Text>
-            <Text fontSize={'sm'}>flo(int v)</Text>
-            <Text fontSize={'sm'}>cel(int v)</Text>
-            <Text fontSize={'sm'}>log(int a, int b)</Text>
-            <Text fontSize={'sm'}>pow(int a, int b)</Text>
-          </Stack>
-
-          <Stack>
-            <HStack justifyContent={"space-between"}>
-              <Text fontSize={'sm'} fontWeight={'semibold'}>自定义函数</Text>
-              <IconButton aria-label={'Add'} size={'sm'} icon={<SmallAddIcon/>}/>
-            </HStack>
-            <Text fontSize={'sm'}>diyFunc1()</Text>
-            <Text fontSize={'sm'}>diyFunc2()</Text>
-          </Stack>
-
-          <Stack>
-            <Text fontSize={'sm'} fontWeight={'semibold'}>鞅函数</Text>
-            <Text fontSize={'sm'}>m1(int pairIndex)</Text>
-            <Text fontSize={'sm'}>m2(int pairIndex)</Text>
-            <Text fontSize={'sm'}>m3(int pairIndex)</Text>
-            <Text fontSize={'sm'}>m4(int pairIndex)</Text>
-            <Text fontSize={'sm'}>m5(int pairIndex)</Text>
-          </Stack>
+        <Stack minW={'400px'} bg={'gray.100'} h={'full'} spacing={4} overflow={'scroll'}>
+          <Accordion defaultIndex={[0, 1, 2]} allowMultiple>
+            {
+              Operator.map((item, index) => (
+                <AccordionItem key={index}>
+                  <AccordionButton>
+                    <Text w={'full'} textAlign={"start"} fontSize={'sm'} fontWeight={'semibold'}
+                          color={'#003434'}>{item.name}</Text>
+                    <AccordionIcon/>
+                  </AccordionButton>
+                  <AccordionPanel px={4} pb={4}>
+                    <Stack spacing={3}>
+                      {
+                        item.children.map((child, childIndex) => (
+                          <Popover key={childIndex} trigger={'hover'}>
+                            <PopoverTrigger>
+                              <Text fontSize={'sm'} cursor={'pointer'} pl={2}
+                                      onClick={() => {
+                                        if (monacoRef) {
+                                          // @ts-ignore
+                                          const selection = monacoRef.current.getSelection();
+                                          const id = { major: 1, minor: 1 };
+                                          const text = child.value + " ";
+                                          const op = {identifier: id, range: selection, text: text, forceMoveMarkers: true};
+                                          // @ts-ignore
+                                          monacoRef.current.executeEdits("my-source", [op]);
+                                        }
+                                      }}>
+                                {child.name}
+                              </Text>
+                            </PopoverTrigger>
+                            <PopoverContent borderRadius={'0'}>
+                              <PopoverHeader fontSize={'sm'} color={'#003232'} fontWeight={'semibold'}>{child.name}</PopoverHeader>
+                              <PopoverBody>
+                                <Text fontSize={'sm'} color={'#003232'} whiteSpace={'pre-wrap'}>{child.explain}</Text>
+                              </PopoverBody>
+                            </PopoverContent>
+                          </Popover>
+                        ))
+                      }
+                    </Stack>
+                  </AccordionPanel>
+                </AccordionItem>
+              ))
+            }
+          </Accordion>
         </Stack>
         <Stack w={'full'} h={'full'} spacing={5}>
           <Stack w={'full'} h={'full'} bg={'#1e1e1e'} py={4}>
@@ -61,7 +191,15 @@ const Home = () => {
               theme={'vs-dark'}
               defaultLanguage={"javascript"}
               defaultValue={""}
-              onChange={(value) => setText(value || '')}
+              onChange={(value) => {
+                setText(value || '')
+              }}
+              onMount={(editor, monaco) => {
+                if (monacoRef) {
+                  // @ts-ignore
+                  monacoRef.current = editor
+                }
+              }}
             />
           </Stack>
           <Stack w={'full'} minH={'240px'} bg={'yellow.200'} p={4} align={"center"} justifyContent={"center"}>
